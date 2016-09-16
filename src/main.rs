a9bcdd39ea0fd1cf3caaf6a2551d642a501e1926
Francis Lalonde <fralalonde@gmail.com>
2016-09-16 16:38:49 -0400

    primitives

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::path::Path;
 use std::env;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::ops::Index;
 
 struct Decomposing<B> {
@@ -13,7 +14,7 @@ struct Decomposing<B> {
 }
 
 trait Decomposable {
-    fn substrings(self, minLen: usize) -> Decomposing<Self> where Self: Sized;
+    fn substrings(self, length: usize) -> Decomposing<Self> where Self: Sized;
 }
 
 impl<'a> Decomposable for &'a str {
@@ -70,32 +71,53 @@ fn main() {
     }
 
     println!("LINES white {} black {}", whitelist.len(), blacklist.len());
+    
+    let MIN_LEN = 3; 
 
-    let mut whitemains: HashMap<&str, usize> = HashMap::new();
+    let mut white_counts: HashSet<&str> = HashSet::new();
     for k in &whitelist {
         // start with smallest substrings, up to full string
-        for i in 3..k.len() {
+        for i in MIN_LEN..k.len() {
             for z in k.substrings(i) {
-                *whitemains.entry(z).or_insert(0) += 1;
+                white_counts.insert(z);
             }
         }
     }
+	
 
-    let mut domains: HashMap<&str, usize> = HashMap::new();
+    println!("[{}] white primitives (exhaustive)", white_counts.len());
+
+    let mut black_counts: HashMap<&str, usize> = HashMap::new();
     for k in &blacklist {
         // start with smallest substrings, up to full string
-        for i in 3..k.len() {
+        for i in MIN_LEN..k.len() {
             for z in k.substrings(i) {
-                if !whitemains.contains_key(z) {
-                    *domains.entry(z).or_insert(0) += 1;
+                if white_counts.contains(z) {
+                    continue;
+                }
+                let mut primitive_exists = false;
+                for j in MIN_LEN..i {
+                    if !primitive_exists {
+                        for x in z.substrings(j) {
+                            if !primitive_exists && black_counts.contains_key(x) {
+                                primitive_exists = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        break;
+                    }
+                }
+                if !primitive_exists {
+                    *black_counts.entry(z).or_insert(0) += 1;
                 }
             }
         }
     }
 
-    println!("UNIQUE SUBSTRINGS {}", domains.len());
+    println!("[{}] black primitives (compact)", black_counts.len());
 
-    let mut sorted_parts: Vec<(&&str, &usize)> = domains.iter().collect();
+    let mut sorted_parts: Vec<(&&str, &usize)> = black_counts.iter().collect();
     sorted_parts.sort_by(|a, b| b.1.cmp(a.1));
 
     for i in 0..20 {
